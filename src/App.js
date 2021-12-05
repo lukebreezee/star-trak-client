d7da5c749383b423a763d062fe6b9a65ccf0a140
lukebreezee <breezeeluke@gmail.com>
2021-12-05 17:36:45 -0500

    Debugging github client login

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ const AppComponent = props => {
       
       !props.userInfo.username
 
-      && /\/register|\/login\/github|\/demo-user-select/.test(location.pathname) === false
+      && /\/register|\/github-login|\/demo-user-select/.test(location.pathname) === false
 
     ) {
   
@@ -89,7 +89,7 @@ const AppComponent = props => {
 
         && location.pathname !== '/create-team'
 
-        && /\/register|\/login\/github|\/login|\/demo-user-select/.test(location.pathname) === false
+        && /\/register|\/github-login|\/login|\/demo-user-select/.test(location.pathname) === false
 
       ) {
   
