997a47fafefb62c85f5ce2537902409dacdcd4f0
lukebreezee <breezeeluke@gmail.com>
2021-12-05 00:45:34 -0500

    Debugging github client login

--- src/App.js
+++ src/App.js
@@ -263,7 +263,7 @@ useEffect(() => {
 
               <Route path="/register/github" component={GithubRegister} />
 
-              <Route path="/login/github/:accessToken" component={GithubLogin} exact />
+              <Route path="/login/github?accessToken=:accessToken" component={GithubLogin} />
 
               <Route path="/register/google" component={GoogleRegister} />
 
