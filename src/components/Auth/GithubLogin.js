5913f13c35528a74377ab620ad040e270fb1a8b3
lukebreezee <breezeeluke@gmail.com>
2021-12-06 01:12:46 -0500

    Debugging github client login

--- src/components/Auth/GithubLogin.js
+++ src/components/Auth/GithubLogin.js
@@ -1,4 +1,4 @@
-import { Route, useParams, useHistory } from 'react-router-dom';
+import { Route, useLocation, useHistory } from 'react-router-dom';
 import { Octokit } from '@octokit/core';
 import { connect } from 'react-redux';
 import { mapCredentials, mapDispatch } from '../../redux/mapToProps';
@@ -7,90 +7,94 @@ import { useEffect } from 'react';
 
 const GithubLoginComponent = props => {
 
-    // // Use params lets us use URL parameters
+    // Use location lets us get the query string from the URL
 
-    // const { accessToken } = useParams();
+    const { search } = useLocation();
 
-    // // Use history allows us to redirect the user
+    // Get the access token from the query string
 
-    // let history = useHistory();
+    const urlParams = new URLSearchParams(search);
 
-    // useEffect(() => {
+    const accessToken = urlParams.get('accessToken');
 
-    //     console.log('useEffect');
+    // Use history allows us to redirect the user
 
-    //     //Fetches user info from back-end
+    let history = useHistory();
 
-    //     const fetchData = async () => {
+    useEffect(() => {
 
-    //         let githubUsername;
+        //Fetches user info from back-end
 
-    //         // Octokit makes it easier to query GitHub's API
+        const fetchData = async () => {
 
-    //         const octokit = new Octokit({ auth: accessToken });
+            let githubUsername;
 
-    //         // Query GitHub for user login
+            // Octokit makes it easier to query GitHub's API
+
+            const octokit = new Octokit({ auth: accessToken });
+
+            // Query GitHub for user login
         
-    //         await octokit.request('GET /user')
+            await octokit.request('GET /user')
         
-    //         .then(res => {
+            .then(res => {
 
-    //             // If res.data.login is truthy, request was successful
+                // If res.data.login is truthy, request was successful
 
-    //             // If falsy, something went wrong, redirect to login
+                // If falsy, something went wrong, redirect to login
         
-    //             if (!res.data.login) {
+                if (!res.data.login) {
 
-    //                 console.log('No login');
+                    console.log('No login');
                     
-    //                 history.push('/login');
+                    history.push('/login');
         
-    //                 return;
+                    return;
                 
-    //             }
+                }
 
-    //             // res.data.login is their username on GitHub
+                // res.data.login is their username on GitHub
         
-    //             githubUsername = res.data.login;
+                githubUsername = res.data.login;
         
-    //         });
+            });
 
-    //         // Fetch user info now that we have the GitHub username
+            // Fetch user info now that we have the GitHub username
         
-    //         axios.post('https://star-trak.herokuapp.com/github-client-login', {
+            axios.post('https://star-trak.herokuapp.com/github-client-login', {
         
-    //             githubUsername
+                githubUsername
         
-    //         })
+            })
             
-    //         .then(res => {
+            .then(res => {
 
-    //             // If res.data.message is truthy, something went wrong
+                // If res.data.message is truthy, something went wrong
         
-    //             if (res.data.message) {
+                if (res.data.message) {
         
-    //                 history.push('/login');
+                    history.push('/login');
         
-    //             }
+                }
 
-    //             // Else, query was successful, so we update user info
+                // Else, query was successful, so we update user info
         
-    //             props.userLogIn(res.data);
+                props.userLogIn(res.data);
 
-    //             // Redirect to projects page
+                // Redirect to projects page
         
-    //             history.push('/projects-admin-pm');
+                history.push('/projects-admin-pm');
         
-    //         });
+            });
 
-    //     };
+        };
 
-    //     // Call the above function
+        // Call the above function
 
-    //     fetchData();
+        fetchData();
 
-    // // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     // This component does not return JSX, we just need it for the URL params
 
@@ -108,20 +112,20 @@ const GithubLoginComponent = props => {
 
 // Connect the above component to redux
 
-const GithubLoginConnected = connect(mapCredentials, mapDispatch)(GithubLoginComponent);
+const GithubLogin = connect(mapCredentials, mapDispatch)(GithubLoginComponent);
 
-const GithubLogin = () => {
+// const GithubLogin = () => {
 
-    return (
+//     return (
 
-        <Route path=":accessToken">
+//         <Route path=":accessToken">
 
-            <GithubLoginConnected />
+//             <GithubLoginConnected />
 
-        </Route>
+//         </Route>
 
-    );
+//     );
 
-};
+// };
 
 export { GithubLogin };
